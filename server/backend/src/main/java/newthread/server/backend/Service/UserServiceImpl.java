321b78e6726bf280e8876ae38af2c5144b4345ca
Fagor <oleg-veber@mail.ru>
2022-12-02 20:12:33 +0700

    implement exception to login and registration

--- server/backend/src/main/java/newthread/server/backend/Service/UserServiceImpl.java
+++ server/backend/src/main/java/newthread/server/backend/Service/UserServiceImpl.java
@@ -2,6 +2,8 @@ package newthread.server.backend.Service;
 
 import newthread.server.backend.Dto.UserDto;
 import newthread.server.backend.Entity.User;
+import newthread.server.backend.Exception.InvalidData;
+import newthread.server.backend.Exception.UserAlreadyExists;
 import newthread.server.backend.Mapper.UserMapper;
 import newthread.server.backend.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,13 +22,17 @@ public class UserServiceImpl implements UserService {
             userRepository.save(userMapper.modelToDto(userDto));
             return true;
         } catch (Exception e) {
-            return false;
+            throw new UserAlreadyExists("UserAlreadyExists");
         }
     }
 
     @Override
     public boolean login(UserDto userDto) {
         User user = userRepository.findFirstByEmailOrLogin(userDto.getEmail(), userDto.getLogin());
-        return user.getPassword().equals(userDto.getPassword());
+        if (user.getPassword().equals(userDto.getPassword())) {
+            return true;
+        } else {
+            throw new InvalidData("InvalidData");
+        }
     }
 }
