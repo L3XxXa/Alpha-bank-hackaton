7870c99b18f0b4054a342de4c9045964cca7c66b
Fagor <oleg-veber@mail.ru>
2022-12-02 14:30:01 +0700

    create userDto

---
+++ server/backend/src/main/java/newthread/server/backend/Dto/UserDto.java
@@ -0,0 +1,11 @@
+package newthread.server.backend.Dto;
+
+import java.awt.*;
+
+public class UserDto {
+    Long id;
+    String login;
+    String email;
+    String password;
+    Point lastGeo;
+}
