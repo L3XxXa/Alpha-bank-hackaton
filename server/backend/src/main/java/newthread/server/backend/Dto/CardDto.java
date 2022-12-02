6e7a7072120b237f38ea775ce741a927d2e30cdb
Fagor <oleg-veber@mail.ru>
2022-12-02 21:11:54 +0700

    create cardDto

--- server/backend/src/main/java/newthread/server/backend/Dto/CardDto.java
+++ server/backend/src/main/java/newthread/server/backend/Dto/CardDto.java
@@ -1,4 +1,13 @@
 package newthread.server.backend.Dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CardDto {
+    String id;
+    String cardName;
+    String cardNumber;
+    String cardImage;
 }
