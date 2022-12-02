9684f6067662e10e366bbf2b6b6ee51739a52674
Fagor <oleg-veber@mail.ru>
2022-12-02 21:17:54 +0700

    migration for inserting test cards

---
+++ server/backend/src/main/resources/db/migration/V4__InsertCards.sql
@@ -0,0 +1,3 @@
+insert into cards (card_name, card_image, card_number,user_id) VALUES ('Пятерочка', 'some_image', '1234567', 1);
+insert into cards (card_name, card_image, card_number,user_id) VALUES ('Лента', 'some_image', '4828418', 1);
+insert into cards (card_name, card_image, card_number,user_id) VALUES ('Горожанка', 'some_image', '4635534534', 1);
