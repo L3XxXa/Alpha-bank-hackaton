f229315d9481342999f3b582657776a2ffed2ed3
L3XxXa <alexkkk@inbox.ru>
2022-12-04 22:20:29 +0700

    Added body

--- app/src/main/java/ru/nsu/hackatonapp/network/ApiService.kt
+++ app/src/main/java/ru/nsu/hackatonapp/network/ApiService.kt
@@ -4,7 +4,9 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Body
 import retrofit2.http.POST
+import ru.nsu.hackatonapp.network.json.login.LoginRequestJson
 import ru.nsu.hackatonapp.network.json.login.LoginResponseJson
 import ru.nsu.hackatonapp.network.json.register.RegisterRequestJson
 
@@ -17,9 +19,9 @@ private val retrofit = Retrofit.Builder()
 
 interface ApiService {
     @POST("/api/user/login")
-    suspend fun loginUser(): LoginResponseJson
+    suspend fun loginUser(@Body loginRequestJson: LoginRequestJson): LoginResponseJson
     @POST("/api/user/register")
-    suspend fun registerUser(): RegisterRequestJson
+    suspend fun registerUser(@Body registerRequestJson: RegisterRequestJson): RegisterRequestJson
 
 }
 
