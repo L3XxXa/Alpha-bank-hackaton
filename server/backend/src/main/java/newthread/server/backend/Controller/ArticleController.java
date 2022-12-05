23647793e51ba024c68a571b22973f08a0535220
Fagor <oleg-veber@mail.ru>
2022-12-05 12:13:19 +0700

    change article controller

--- server/backend/src/main/java/newthread/server/backend/Controller/ArticleController.java
+++ server/backend/src/main/java/newthread/server/backend/Controller/ArticleController.java
@@ -3,8 +3,7 @@ package newthread.server.backend.Controller;
 import newthread.server.backend.Exception.InvalidData;
 import newthread.server.backend.Exception.NotFound;
 import newthread.server.backend.Exception.UserAlreadyExists;
-import newthread.server.backend.Utils.BadResponse;
-import newthread.server.backend.Utils.SuccessResponse;
+import newthread.server.backend.Utils.BaseResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,8 +12,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ArticleController {
     @ExceptionHandler({UserAlreadyExists.class, InvalidData.class, NotFound.class})
-    public <T extends RuntimeException> ResponseEntity<SuccessResponse<Boolean>> handleException(T e) {
-        return new ResponseEntity<>(new SuccessResponse<>(false,
+    public <T extends RuntimeException> ResponseEntity<BaseResponse<Boolean>> handleException(T e) {
+        return new ResponseEntity<>(new BaseResponse<>(false,
                 e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
