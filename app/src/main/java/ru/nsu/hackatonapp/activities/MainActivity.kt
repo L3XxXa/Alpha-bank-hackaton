f206fffe191436b1d6b71734447c810c8af77012
L3XxXa <alexkkk@inbox.ru>
2022-12-07 01:28:19 +0700

    Some staff

--- app/src/main/java/ru/nsu/hackatonapp/activities/MainActivity.kt
+++ app/src/main/java/ru/nsu/hackatonapp/activities/MainActivity.kt
@@ -1,16 +1,27 @@
 package ru.nsu.hackatonapp.activities
 
 import android.Manifest
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View.*
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import ru.nsu.hackatonapp.databinding.ActivityMainBinding
 import ru.nsu.hackatonapp.utils.LogTags
 import android.widget.Toast
+import androidx.databinding.BindingAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import ru.nsu.hackatonapp.R
 import ru.nsu.hackatonapp.domain.viewmodels.LoginViewModel
 import ru.nsu.hackatonapp.network.BaseResponse
+import java.io.Console
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -22,6 +33,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         askForPermission()
+        loginObserver()
+        binding.loginButton.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun loginObserver() {
         loginViewModel.loginResult.observe(this) {
             when (it) {
                 is BaseResponse.Loading -> {
@@ -41,16 +59,51 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-        binding.loginButton.setOnClickListener {
-            login()
-        }
     }
 
     private fun login() {
         val email = binding.loginEmail.text.toString()
         val password = binding.loginPassword.text.toString()
+        if (!checkEmail(binding.loginEmail.text.toString())){
+            if (!checkPassword(binding.loginPassword.text.toString())){
+                Log.i(LogTags.LOGIN_TAG, "Not password")
+                return
+            }
+            Log.i(LogTags.LOGIN_TAG, "Not email")
+            return
+        }
+        binding.errorEmailLogin.visibility = INVISIBLE
+        if (!checkPassword(binding.loginPassword.text.toString())){
+            Log.i(LogTags.LOGIN_TAG, "Not password")
+            return
+        }
+        binding.errorPswdLogin.visibility = INVISIBLE
         loginViewModel.loginUser(email, password)
     }
+
+    private fun checkPassword(password: String): Boolean {
+        if(password == ""){
+            displayError(binding.errorPswdLogin, getString(R.string.enter_password))
+            return false
+        }
+        return true
+    }
+
+    private fun checkEmail(email: String): Boolean {
+        val regex = "^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})"
+        if(!regex.toRegex().matches(email)){
+            displayError(binding.errorEmailLogin, getString(R.string.enter_valid_email))
+            return false
+        }
+        return true
+    }
+
+
+    private fun displayError(textView: TextView, text: String) {
+        textView.text = text
+        textView.visibility = VISIBLE
+    }
+
 
     private fun askForPermission() {
         Log.i(LogTags.PERMISSION_TAG, "Asking for permission")
