282eade6c42fd9bb5d6e9cd6b44d78ea72597fd9
Fagor <oleg-veber@mail.ru>
2022-12-08 15:28:34 +0700

    delete image

--- server/backend/src/main/java/newthread/server/backend/Entity/Card.java
+++ server/backend/src/main/java/newthread/server/backend/Entity/Card.java
@@ -14,7 +14,6 @@ public class Card {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String image;
     private String number;
     private Integer priority;
 }
