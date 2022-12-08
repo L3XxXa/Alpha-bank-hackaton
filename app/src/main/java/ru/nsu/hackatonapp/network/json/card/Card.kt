40ea3abeefc5d18f17ce8cd726fac8ab0cd84e97
L3XxXa <alexkkk@inbox.ru>
2022-12-08 13:11:39 +0700

    Card entity

---
+++ app/src/main/java/ru/nsu/hackatonapp/network/json/card/Card.kt
@@ -0,0 +1,5 @@
+package ru.nsu.hackatonapp.network.json.card
+
+import com.google.gson.annotations.SerializedName
+
+data class Card(@SerializedName("name") val name: String, @SerializedName("number") val number: String)
